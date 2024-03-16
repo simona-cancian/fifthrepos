23abba2b5765c2ab7326972f3d34b2c0d2b92585
Simona Cancian <simona.cancian@student.tafesa.edu.au>
2024-03-16 13:52:26 +1030

    initial loading of files

--- BankingApp/BankingApp/Properties/Settings.Designer.cs
+++ BankingApp/BankingApp/Properties/Settings.Designer.cs
@@ -6,20 +6,23 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
-//------------------
-//------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-namespace Ticketing.Properties {
-    
-    
+namespace BankingApp.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
